32877bdd408e140eb95892ad26eb4791e002230f
choimory <choimory@naver.com>
2021-09-22 11:36:59 +0900

    컬럼 업데이트 수정, 맥스카운트 설정

--- src/main/java/com/choimory/springbatchinaction/common/entity/CommonDateTimeEntity.java
+++ src/main/java/com/choimory/springbatchinaction/common/entity/CommonDateTimeEntity.java
@@ -9,6 +9,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -22,6 +23,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public abstract class CommonDateTimeEntity {
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createDateTime;
     @LastModifiedDate
     private LocalDateTime updateDateTime;
